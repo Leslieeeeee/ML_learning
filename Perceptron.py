7f432545262c21af0a129a4c09b2d89aac695478
Leslieeeeee <Leslieeeeee@users.noreply.github.com>
2017-04-17 16:11:33 +0800

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -1,59 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 22:18:32 2017
 
-# coding: utf-8
-
-# In[ ]:
-
+@author: leslieeeeee
+"""
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
-class Perceptron(object):
+
+class Neu_Net(object):
     #初始化函数
-    def __int__(self, rate = 0.01, n = 10):
+    def __init__(self, rate = 0.01, n = 50):
         self.rate = rate
         self.n = n
         
     #输入样本和一个期望输出
     #初始化权重向量
-    def Input_w(self, x):
+    def Input_w(self, x, target):
         #x:shape[n_samples,n_features]
-        xi = x.shape(1)
-        self.w = np.zeros(1 + xi)
-        w = self.w
-        times = 0
-        
-        #重复训练十次
-        for x in range(self.n):  
-            
+        #初始化为0
+        self.w = np.zeros(1 + x.shape[1])
+        self.const_ = []
+        #重复训练n次
+        for i in range(self.n): 
+            k = self.Cal(x)
             #求出误差并与期望输出相比较
-            #更新权值
-            for x, target in add_up(x,y):
-                new_e = self.rate * (target - Cal(xi))
-                for i in w:
-                    w[0] += new_e
-                    w[1:] = new_e * xi
-                
-                if new_e = 0.0:
-                    break
-                else:
-                    times ++
-            print "This new curve is：%d", y
-            print "err times is: %d", times
-            
-             
-            
+            new_e= (target - k)              
+            self.w[0] += self.rate * new_e.sum()
+            #求出权值变化值再更新
+            self.w[1:] += self.rate * x.T.dot(new_e)
+            cost = (new_e ** 2).sum()/2.0
+            self.const_.append(cost)
+        return self
     
-    #计算各层输出
+   
+    #计算输出
     def Cal(self, x):
-        y_before = w[1:].np.T * x
-        # y = np.dot(w[1:], x) + w[0]
-        y_mid = y_before.sum()
-        if y_mid >= 0.0:
-            y = 1
-        else:
-            y = -1
-        return y
-    
-    def add_up(self, x, y):
-        self.x = x 
-        self.y = y
-        hstack(x, y)
+        return np.dot(x, self.w[1:]) + self.w[0]
+        #y = self.w[1:].np.T * x + self.w[0]
         
-
+    def activation():
+        pass
+    
+    def sort(self, x):
+        return np.where(self.Cal(x) >= 0.0 , -1, 1)
